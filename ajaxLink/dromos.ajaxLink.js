8965dafcbd4dfc5a8983b67773e84adb8509d77e
Ken McHugh <kmchugh@icatalyst.co.uk>
2012-09-07 14:26:44 +0800

    Updates for ajax requests

--- ajaxLink/dromos.ajaxLink.js
+++ ajaxLink/dromos.ajaxLink.js
@@ -19,7 +19,13 @@ define(["jquery"], function($jQ)
              * This occurs when the user moves the mouse over the link.
              * @param  Event toEvent the event that occurred
              */
-            onStartHover : function(toEvent){/* By default this takes no action */},
+            onStartHover : function(toEvent)
+            {
+                if (this._targetContainer)
+                {
+                    this._targetContainer.css({"display" : "block"});
+                }
+            },
             /**
              * This occurs when the user moves the mouse off the link.
              * @param  Event toEvent the event that occurred
@@ -30,15 +36,17 @@ define(["jquery"], function($jQ)
              */
             onTimeout : function(){alert('oops you timed out');},
             // The link to make the ajax request to, if false the ajax request will be made to the anchor href (default)
-            link : false,
+            url : false,
             // The target, the result of the ajax call will be loaded into the target, if the target is false then a popup will be created to host the content (default)
             target : false,
             // The method to use to  make the ajax call, "GET" is the default
             method : 'GET',
             // The event to bind to for loading the ajax content, click is default
-            loadEvent: 'click',
+            loadEvent: 'mouseover',
             // Used for jsonp callbacks
             callbackMarker : "callback",
+            // Flag to state if the user can click on the link to go through to the original url anyway
+            preventClickThrough : false,
             // Expected return type
             dataType : "html",
             // The max wait before timing out the ajax request
@@ -47,7 +55,14 @@ define(["jquery"], function($jQ)
             cache : true,
             // The default content type that will be sent to the server for POST requests
             contentType : "application/x-www-form-urlencoded; charset=UTF-8",
-            contentTemplate : "<div class=\"ajaxLinkWindow\"><div class=\"content\"></div></div>"
+            // Flag to state if content should be reloaded once it has been loaded once
+            allowReload : false,
+            // The class that will be used to decorate the container
+            containerClass : "ajaxLinkWindow",
+            // The template to use when pushing content to _blank.  The content will be placed in the element with the content class
+            contentTemplate : "<div><div class=\"content\"></div></div>",
+            // The data to send as part of the request
+            data : null,
 
             // TODO :Load image
             // TODO: load title
@@ -60,22 +75,30 @@ define(["jquery"], function($jQ)
             // TODO: onRequestStarted
             // TODO: onRequestTimedOut
             // TODO: once content loading/loaded, don't attempt to reload
-            // 
+            //
             // preload link
             // todo: use target as where to load the ajax result
         },
         _create : function()
         {
-            this.element.css({"background" : "red"});
+            var loJQElement = $jQ(this.element);
+            this.options.url = this.options.url || loJQElement.attr('href');
+            loJQElement.attr("href", "#");
+
             // Bind to the loadEvent click and hover events
-            $jQ(this.element).bind(this.options.loadEvent, dromos.utilities.createCallback(
+            loJQElement.bind(this.options.loadEvent, dromos.utilities.createCallback(
                 function(toEvent)
                 {
-                    toEvent.preventDefault();
                     this._loadContent();
                 }, this)).click(dromos.utilities.createCallback(function(toEvent)
                 {
-                    toEvent.preventDefault();
+                    if (toEvent.target == this.element[0])
+                    {
+                        if (!this.options.preventClickThrough)
+                        {
+                            window.location = this.options.url;
+                        }
+                    }
                     this.options.onClick.call(this, toEvent);
                 }, this)).hover(dromos.utilities.createCallback(function(toEvent)
                 {
@@ -94,15 +117,13 @@ define(["jquery"], function($jQ)
          */
         _loadContent : function()
         {
-            if (!this._isLoading)
+            if (!this._isLoading && (this._content == null || this.options.allowReload))
             {
                 this._isLoading = true;
 
-                var lcURL = this.getUrl();
-
                 $jQ.ajax({
                     type: this.options.method,
-                    url: this.getUrl(),
+                    url: this.options.url,
                     dataType: this.options.dataType,
                     jsonp:this.options.callbackMarker,
                     global: false,
@@ -110,6 +131,7 @@ define(["jquery"], function($jQ)
                     cache : this.options.cache,
                     contentType : this.options.contentType,
                     context: this,
+                    data : this.options.data,
                     complete : dromos.utilities.createCallback(
                             function(toXHR, tcStatus)
                             {
@@ -127,49 +149,62 @@ define(["jquery"], function($jQ)
                             function(toData, tcStatus, toXHR)
                             {
                                 this.onSuccess(toData);
-                            }, this),
-                    beforeSend: dromos.utilities.createCallback(
-                        function(toXHR, taSettings)
-                        {
-                            console.error("Before send");
-                            console.error(toXHR);
-                            console.error(taSettings);
-                        }, this)
+                            }, this)
                 });
             }
         },
         // Flag to indicate if a request is underway
         _isLoading : false,
-        // Gets the URL that is to be requested by the ajax request
-        getUrl : function(){return this.options.url || $jQ(this.element).attr('href');},
+        _target : false,
+        _targetContainer : false,
+        _containerFocus : false,
+        
         // Gets the target for the successful ajax request
         getTarget : function(){return this.options.target || $jQ(this.element).attr('target') || "_blank";},
         // Occurs only when the ajax request has been successful
         onSuccess : function(toData)
         {
+            this._content = toData;
             var lcTarget = this.getTarget();
             if (lcTarget === "_blank")
             {
                 // Create the window
                 if (!this._target)
                 {
-                    this._target = $jQ(this.options.contentTemplate);
-                    $jQ(this.element).after(this._target);
-                    this._target = $jQ(".content", this._target);
+                    this._targetContainer = $jQ(this.options.contentTemplate).addClass(this.options.containerClass);
+                    this._targetContainer.addClass(this.options.url.split('/')[1]);
+                    $jQ(this.element).append(this._targetContainer);
+                    this._target = $jQ(".content", this._targetContainer);
+
+                    this._targetContainer.hover(dromos.utilities.createCallback(function(toEvent){
+                        this._containerFous = true;
+
+                    }, this), dromos.utilities.createCallback(function(toEvent){
+                        if (this._containerFous)
+                        {
+                            this._targetContainer.css({"display" : "none"});
+                            this._containerFous = false;
+                        }
+                    }, this));
+
+                    this._targetContainer.click(dromos.utilities.createCallback(function(toEvent){
+                        toEvent.stopImmediatePropagation();
+                        toEvent.bubble = false;
+                    }, this));
                 }
             }
             else
             {
                 this._target = $jQ(this.getTarget());
             }
-            $jQ(this._target).html(toData);
+            $jQ(this._target).html(this._content);
         },
         // Occurs only when an error occurs with the ajax request
         onError : function(tcError){console.error('ERROR'); console.error(tcError);},
         // Occurs after the ajax request has completed, either by error or by success
-        onComplete : function(){console.error('COMPLETE');},
+        onComplete : function(){},
         // Occurs after the ajax request has timed out
-        onTimeout : function(){console.error('TIMEOUT');}
+        onTimeout : function(){}
     });
 
     dromos.ajaxLink =
